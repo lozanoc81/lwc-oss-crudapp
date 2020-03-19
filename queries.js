33a8837f42a648c2232ac004a64babe200ef79e1
Leonardo Cortes <cleonardo21@gmail.com>
2020-03-18 23:53:20 -0600

    API Identidades

---
+++ queries.js
@@ -0,0 +1,107 @@
+var promise = require('bluebird');
+
+var options = {
+    // Initialization Options
+    promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = process.env.DATABASE_URL;
+console.log('connectionString=' + connectionString);
+var db = pgp(connectionString);
+
+function getAllIdentidades(req, res, next) {
+    db.any('select * from identidad')
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Retrieved ALL Identidades'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+function getSingleIdentidad(req, res, next) {
+    var idenID = parseInt(req.params.id);
+    db.one('select * from identidad where id = $1', idenID)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Retrieved ONE Identidad'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+function createIdentidad(req, res, next) {
+    console.log('req=' + JSON.stringify(req.body));
+    req.body.id = parseInt(req.body.id);
+    db.none(
+        'INSERT INTO identidad(id, ine, pasaporte, nombre, apellido_paterno, apellido_materno, vigente) ' +
+            'VALUES(${id}, ${ine}, ${pasaporte}, ${nombre}, ${apellido_paterno}, ${apellido_materno}, ${vigente})',
+        req.body
+    )
+        .then(function() {
+            res.status(200).json({
+                status: 'success',
+                message: 'Inserted one Identidad'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+function updateIdentidad(req, res, next) {
+    db.none(
+        'update identidad set ine=$1, pasaporte=$2, nombre=$3, apellido_paterno=$4, apellido_materno=$5, vigente=$6 where id=$7',
+        [
+            req.body.ine,
+            req.body.pasaporte,
+            req.body.nombre,
+            req.body.apellido_paterno,
+            req.body.apellido_materno,
+            req.body.vigente,
+            parseInt(req.params.id)
+        ]
+    )
+        .then(function() {
+            res.status(200).json({
+                status: 'success',
+                message: 'Updated Identidad'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+function removeIdentidad(req, res, next) {
+    var idenID = parseInt(req.params.id);
+    db.result('delete from identidad where id = $1', idenID)
+        .then(function(result) {
+            /* jshint ignore:start */
+            res.status(200).json({
+                status: 'success',
+                message: `Removed ${result.rowCount} Identidad`
+            });
+            /* jshint ignore:end */
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+module.exports = {
+    getAllIdentidades: getAllIdentidades,
+    getSingleIdentidad: getSingleIdentidad,
+    createIdentidad: createIdentidad,
+    updateIdentidad: updateIdentidad,
+    removeIdentidad: removeIdentidad
+};
--- scripts/server.js
+++ scripts/server.js
@@ -2,22 +2,35 @@
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = require('../queries');
+
 const path = require('path');
 
 const app = express();
 app.use(helmet());
 app.use(compression());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3001;
 const DIST_DIR = './dist';
 
-app.use(express.static(DIST_DIR));
+const router = express.Router();
+router.get('/api/identidades', db.getAllIdentidades);
+router.get('/api/identidades/:id', db.getSingleIdentidad);
+router.post('/api/identidades', db.createIdentidad);
+router.put('/api/identidades/:id', db.updateIdentidad);
+router.delete('/api/identidades/:id', db.removeIdentidad);
 
+app.use(express.static(DIST_DIR));
+app.use('/', router);
 app.use('*', (req, res) => {
     res.sendFile(path.resolve(DIST_DIR, 'index.html'));
 });
-
-app.listen(PORT, () =>
-    console.log(`✅  Server started: http://${HOST}:${PORT}`)
-);
+app.listen(PORT, () => console.log(` Server started: http://${HOST}:${PORT}`));
